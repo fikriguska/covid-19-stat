117381ce56c78164b781ecd3b71b75b355897333
fikriguska <bintangf00code@gmail.com>
2020-05-31 15:26:09 +0700

    country not found

--- src/script/components/cards-covid.js
+++ src/script/components/cards-covid.js
@@ -33,43 +33,54 @@ class cardsCovid extends HTMLElement {
         .then(response => response.json())
         .then(country => {
             let update = getDatetime(country.lastUpdate);
-            this.innerHTML = `
-                <div class="row mt-3 table-country text-white font-weight-bold">
-                    <div class="col-md-4 my-1">
-                        <div class="card border-0 bg-warning">
-                            <div class="card-body">
-                              <h5 class="card-title text-center">Terinfeksi</h5>
-                              <!-- <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6> -->
-                              <p class="card-text text-center display-number">${separator(country.confirmed.value)}</p>
-                            </div>
+            if(country.error != undefined){
+                this.innerHTML = `
+                    <div class="row mt-3">
+                        <div class="col text-center">
+                        Data negara tidak ditemukan
                         </div>
                     </div>
-                    <div class="col-md-4 my-1">
-                        <div class="card border-0 bg-danger">
-                            <div class="card-body">
-                              <h5 class="card-title text-center">Meninggal</h5>
-                              <!-- <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6> -->
-                              <p class="card-text text-center display-number">${separator(country.deaths.value)}</p>
+                `
+            }
+            else{
+                this.innerHTML = `
+                    <div class="row mt-3 table-country text-white font-weight-bold">
+                        <div class="col-md-4 my-1">
+                            <div class="card border-0 bg-warning">
+                                <div class="card-body">
+                                  <h5 class="card-title text-center">Terinfeksi</h5>
+                                  <!-- <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6> -->
+                                  <p class="card-text text-center display-number">${separator(country.confirmed.value)}</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-4 my-1">
+                            <div class="card border-0 bg-danger">
+                                <div class="card-body">
+                                  <h5 class="card-title text-center">Meninggal</h5>
+                                  <!-- <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6> -->
+                                  <p class="card-text text-center display-number">${separator(country.deaths.value)}</p>
 
+                                </div>
                             </div>
                         </div>
-                    </div>
-                    <div class="col-md-4 my-1">
-                        <div class="card border-0 bg-success">
-                            <div class="card-body">
-                              <h5 class="card-title text-center">Sembuh</h5>
-                              <!-- <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6> -->
-                              <p class="card-text text-center display-number">${separator(country.recovered.value)}</p>
+                        <div class="col-md-4 my-1">
+                            <div class="card border-0 bg-success">
+                                <div class="card-body">
+                                  <h5 class="card-title text-center">Sembuh</h5>
+                                  <!-- <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6> -->
+                                  <p class="card-text text-center display-number">${separator(country.recovered.value)}</p>
 
+                                </div>
                             </div>
                         </div>
+                        <div class="col text-center text-dark mt-2">
+                            pembaharuan terakhir: ${update}
+                        </div>
                     </div>
-                    <div class="col text-center text-dark mt-2">
-                        pembaharuan terakhir: ${update}
-                    </div>
-                </div>
-                
-            `;
+
+                `;
+            }
         });
 
     }
