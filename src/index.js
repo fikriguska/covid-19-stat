e6520bbdf307347fbc885ae466795aee55951dfe
fikriguska <bintangf00code@gmail.com>
2020-05-31 13:28:01 +0700

    rollback

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import "./style/bootstrap.min.css";
 import "./style/style.css";
-import  $ from "./script/jquery.slim.min.js";
 import main from "./script/main.js";
 
 document.addEventListener("DOMContentLoaded", main);
